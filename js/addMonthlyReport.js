e7a01a900d17d902378ff3829b075c0e67b90cc6
encprojectmoses <encprojectmoses@gmail.com>
2017-08-08 16:45:25 +0800

    Additional logic for textarea and JS - Add Monthly

--- js/addMonthlyReport.js
+++ js/addMonthlyReport.js
@@ -6,33 +6,71 @@ var selectedYear = 				"";
 var youthServiceAttendance = 	0;
 var aveTnoCount = 				0;
 
-function selectMonth(month)
-{
+function selectMonth(month){
 	if(month != 'Choose Month...')
 		selectedMonth = $("#input_date_month").find("option:selected").text();
 }
 
-function selectYear(year)
-{
+function selectYear(year){
 	if(year != 'Choose Year...')
 		selectedYear = $("#input_date_year").find("option:selected").text();
 }
 
-function setYouthServiceAttendance(value)
-{
+function setYouthServiceAttendance(value){
 	if(value != null || value != "")
 		youthServiceAttendance = document.getElementById("youth_service_attendance").value;
 }
 
-function setAveTnoCount(value)
-{
+function setAveTnoCount(value){
 	if(value != null || value != "")
 		aveTnoCount = document.getElementById("ave_tno_count").value;
 }
 
-//Submit Button
-$(function() {
-	
-});
+function isOtherSelected(value)
+{
+	if(value != "Other")
+		document.getElementById("").style.visibility="hidden";  
+		
+}
 
+function convertFormContentToJson(user)
+{
+	var campus_story = $('#campus_outreach_textarea').val();
+	var student_story = $('#student_leadership_textarea').val();
+	var leader_story = $('#leadership_development_textarea').val();
+	
+	var jsonData = {
+		youth_service_attendance: youthServiceAttendance,
+		tno_envelope: aveTnoCount,
+		campus_outreach_story_classification: , 
+		campus_outreach_story_other_title: , 
+		campus_outreach_story: campus_story, 
+		student_discipleship_classification:, 
+		student_discipleship_other_title:, 
+		student_discipleship_story: student_story, 
+		leadership_development_classification:, 
+		leadership_development_other_title:, 
+		leadership_development_story: leader_story, 
+		created_by: user, 
+		created_date:, 
+		last_mod_date:,
+		month_report: selectedMonth + " " selectedYear,
+	};
+	
+	return jsonData;
+}
+
+function submitButton(){
+	
+	/*$.ajax({
+		type: "POST",
+		url: "../php/addMonthly.php",
+		data: ,//how to get form here
+		cache: false,
+		success: function(){
+			
+		},
+			
+	});*/
+}
 //TODO: Form Validation - Mandatory Fields
